8156166e0b59e4a49ee498707a98348229ab4e72
Mike Patterson <m.w.patterson@live.com>
2023-10-10 12:48:18 -0400

    Optimized Script

--- GPOBackup - Export Only.ps1
+++ GPOBackup - Export Only.ps1
@@ -167,12 +167,21 @@ function IsNotLinked($xmldata) {
 }
 $unlinkedGPOs = @()
 If ($setServer -eq "Yes") {
-    Get-GPO -All -Server $server | ForEach-Object { $gpo = $_ ; $_ | Get-GPOReport -Server $server -ReportType xml | ForEach-Object { If (IsNotLinked([xml]$_)) { $unlinkedGPOs += $gpo } } }
+    #Get-GPO -All -Server $server | ForEach-Object { $gpo = $_ ; $_ | Get-GPOReport -Server $server -ReportType xml | ForEach-Object { If (IsNotLinked([xml]$_)) { $unlinkedGPOs += $gpo } } }
     #$Script:GPOs | ForEach-Object { $gpo = $_ ; $_ | Get-GPOReport -Server $server -ReportType xml | ForEach-Object { If (IsNotLinked([xml]$_)) { $unlinkedGPOs += $gpo } } }
+    foreach ($gpo in $Script:GPOs) {
+        #Get-GPOReport -Name $gpo.displayname -Server $server -ReportType xml | ForEach-Object { If (IsNotLinked([xml]$_)) { $unlinkedGPOs += $gpo } }
+        Get-GPOReport -Guid $gpo.Id -Server $server -ReportType xml | ForEach-Object { If (IsNotLinked([xml]$_)) { $unlinkedGPOs += $gpo } }
+    }
 }
 Else {
-    Get-GPO -All | ForEach-Object { $gpo = $_ ; $_ | Get-GPOReport -ReportType xml | ForEach-Object { If (IsNotLinked([xml]$_)) { $unlinkedGPOs += $gpo } } }
+    #Get-GPO -All | ForEach-Object { $gpo = $_ ; $_ | Get-GPOReport -ReportType xml | ForEach-Object { If (IsNotLinked([xml]$_)) { $unlinkedGPOs += $gpo } } }
     #$Script:GPOs | ForEach-Object { $gpo = $_ ; $_ | Get-GPOReport -ReportType xml | ForEach-Object { If (IsNotLinked([xml]$_)) { $unlinkedGPOs += $gpo } } }
+    foreach ($gpo in $Script:GPOs) {
+        #Get-GPOReport -Name $gpo.displayname -ReportType xml | ForEach-Object { If (IsNotLinked([xml]$_)) { $unlinkedGPOs += $gpo } }
+        Get-GPOReport -Guid $gpo.Id -ReportType xml | ForEach-Object { If (IsNotLinked([xml]$_)) { $unlinkedGPOs += $gpo } }
+    }
+
 }
 If ($unlinkedGPOs.Count -eq 0) {
     Write-Host "`t`tNo Unlinked GPO's Found" -ForeGroundColor Green
